717975338c8a1d84d5140df0b7ab6ce228e45ae9
Zachary Alexander <zachal@microsoft.com>
2015-08-21 14:20:39 -0700

    Initial Commit

---
+++ unencryptedCredentials.ps1
@@ -0,0 +1,107 @@
+﻿#Prompt user for their credentials
+#credentials will be unencrypted in the MOF
+$promptedCreds = get-credential -Message "Please enter your credentials to generate a DSC MOF:"
+
+#Store passwords in plaintext, in the document itself
+#will also be stored in plaintext in the mof
+$password = "ThisIsAPlaintextPassword" | ConvertTo-SecureString -asPlainText -Force
+$username = "badIdea"
+[PSCredential] $credential = New-Object System.Management.Automation.PSCredential($username,$password)
+
+#DSC requires explicit confirmation before storing passwords insecurely
+$ConfigurationData = @{
+    AllNodes = @(
+        @{
+            #the "*" means "all nodes named in ConfigData" so we don't have to repeat ourselves
+            NodeName="*"
+            PSDscAllowPlainTextPassword = $true
+        },
+        #however, each node still needs to be explicitly defined for "*" to have meaning
+        @{
+            NodeName = "srv4-10264-831a"
+        },
+        #we can also use a property to define node-specific passwords, although this is no more secure
+        @{
+            NodeName = "srv4-10264-832a";
+            UserName = "zach"
+            LocalPass = "ThisIsYetAnotherPlaintextPassword"
+        }
+        )
+}
+configuration unencryptedPasswordDemo
+{
+    Node "srv4-10264-831a"
+    {
+        #we use the plaintext password to generate a new account
+        User zach
+        {
+            UserName = $username
+            Password = $credential
+            Description = "local account"
+            Ensure = "Present"
+            Disabled = $false
+            PasswordNeverExpires = $true
+            PasswordChangeRequired = $false
+        }
+        #and we use the prompted password to add this account to the local admins group
+        Group addToAdmin
+        {
+            #we need to ensure the user exists before we add him to a group
+            DependsOn = "[User]zach"
+            Credential = $promptedCreds
+            GroupName = "Administrators"
+            Ensure = "Present"
+            MembersToInclude = "zach"
+        }
+
+      Log test
+      {
+        Message = "hi"
+      }  
+    }
+
+    
+    Node "srv4-10264-832a"
+    {
+        #now let's allocate the node-specific password to this machine
+        $password = $Node.LocalPass | ConvertTo-SecureString -asPlainText -Force
+        $username = $node.UserName
+        [PSCredential] $nodeCred = New-Object System.Management.Automation.PSCredential($username,$password)
+
+        User zach
+        {
+            UserName = $username
+            Password = $nodeCred
+            Description = "local account"
+            Ensure = "Present"
+            Disabled = $false
+            PasswordNeverExpires = $true
+            PasswordChangeRequired = $false
+        }
+
+        Group addToAdmin
+        {
+            Credential = $domain
+            GroupName = "Administrators"
+            DependsOn = "[User]zach"
+            Ensure = "Present"
+            MembersToInclude = "zach"
+        }
+      
+      Log test
+      {
+        Message = "hi"
+      } 
+    }
+
+
+}
+#We declared the configurationData in a local variable, but we need to pass it in to our configuration function
+#We need to invoke the configuration function we created to generate a MOF
+unencryptedPasswordDemo -ConfigurationData $ConfigurationData
+#we need to pass the MOF to the machines we named.
+#-wait: doesn't use jobs so we get blocked at the prompt until the configuration is done
+#-verbose: so we can see what's going on and catch any errors
+#-force: for testing purposes, I run start-dscconfiguration frequently + want to make sure i'm
+#        not blocked by previous configurations that are still running
+Start-DscConfiguration ./sqlDemo -verbose -wait -force
